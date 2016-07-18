d4399de4035f0b719a8afe16793730bc39151086
HangXuan <xuan.hang@mi-me.com>
2016-07-18 14:28:46 +0800

    Solved the problem that in some cases it reported 'unexpected token import'

--- src/Datepicker.vue
+++ src/Datepicker.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script>
-import Datepicker from './Datepicker.js'
+import { init as Flatpickr } from 'flatpickr'
+
+function Datepicker (selector, config, l10n) {
+  this.l10n = Object.assign({}, Flatpickr.prototype.l10n, l10n)
+  return Flatpickr.call(this, selector, config)
+}
+
+Datepicker.prototype = Flatpickr.prototype
 
 export default {
   partials: {
